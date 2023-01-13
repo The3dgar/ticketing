cb93bdaaf03011514700ee7a2c424d21d4abcad1
edgar <edgarolivar16@gmail.com>
2023-01-12 23:18:37 -0300

    update hostname

--- services/src/auth/app.ts
+++ services/src/auth/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
-import { cookieSessionMiddleware, ErrorHandlerMiddleware, NotFoundError } from '@eotickets/common';
+import { ErrorHandlerMiddleware, NotFoundError } from '@eotickets/common';
+import cookieSession from "cookie-session";
 
 import { authRouter } from './routes/auth-routes';
 
@@ -12,7 +13,12 @@ app.set('trust proxy', true);
 
 app.use(cors());
 app.use(express.json());
-app.use(cookieSessionMiddleware);
+app.use(
+  cookieSession({
+    signed: false,
+    secure: false,
+  })
+);
 
 app.all('*', (req, _, next) => {
   console.log('Request:', req.method, req.path, new Date());
--- services/src/orders/app.ts
+++ services/src/orders/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
-import { cookieSessionMiddleware, CurrentUserMiddleware, ErrorHandlerMiddleware, NotFoundError } from '@eotickets/common';
+import { CurrentUserMiddleware, ErrorHandlerMiddleware, NotFoundError } from '@eotickets/common';
+import cookieSession from "cookie-session";
 
 import { orderRouter } from './routes/orders-routes';
 
@@ -12,7 +13,12 @@ app.set('trust proxy', true);
 
 app.use(cors());
 app.use(express.json());
-app.use(cookieSessionMiddleware);
+app.use(
+  cookieSession({
+    signed: false,
+    secure: false,
+  })
+);
 
 app.all('*', (req, _, next) => {
   console.log('Request: ', req.method, req.path, new Date());
